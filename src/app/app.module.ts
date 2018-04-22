dca64b35a1a190d9abca71e7c8457eb587f8f1fa
Daniel Hulse <danielhulse@Daniels-MacBook-Pro.local>
2018-04-22 17:32:34 +0100

    Added simple sign up form. Needs functionality and test coverage.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
-
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SignupFormComponent } from "./signup-form/signup-form.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
